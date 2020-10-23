002a1d1722c7fae2639312ca77d2d2c310b3cfa2
Nguyen Nam <namnv@kis-v.com.vn>
2020-10-23 14:49:58 +0700

    build web

---
+++ .dart_tool/flutter_build/2e92762ab3d42ba00229c0f444908af8/main.dart
@@ -0,0 +1,16 @@
+// @dart = null
+
+import 'dart:ui' as ui;
+
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+import 'package:Fluttergram/generated_plugin_registrant.dart';
+import 'package:Fluttergram/main.dart' as entrypoint;
+
+Future<void> main() async {
+  registerPlugins(webPluginRegistry);
+  if (true) {
+    await ui.webOnlyInitializePlatform();
+  }
+  entrypoint.main();
+}
